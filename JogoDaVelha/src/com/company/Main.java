a9412e24d4be993df2d57ce3fc75d73c58788038
MateusKekw <126785833+MateusKekw@users.noreply.github.com>
2023-03-09 11:47:17 -0300

    Add files via upload

---
+++ JogoDaVelha/src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int tabuleiro[][] = new int [3][3];
+
+
+        System.out.println("Jogo da Velha");
+        System.out.printf("Faça uma linha em qualquer direção para ganhar (2 jogadores)");
+
+
+        public static boolean vitoria;(char[][] tabuleiro){
+            tabuleiro[0][0];
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    tabuleiro[i][j];
+                }
+            }
+        }
+            for (char[] linha: tabuleiro) {
+                for (char posicao:linha){
+                    posicao;
+                }
+            }
+        }
+
+
+
+    }
+
